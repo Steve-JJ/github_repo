8e70d339484276626eaedddebd2efad1a440567b
Stephen Jeffrey <stephen.jeffrey@qld.gov.au>
2021-08-12 13:04:48 +1000

    Add a new file (file 1)

---
+++ file_1.cc
@@ -0,0 +1,10 @@
+File 1, Line 0 contains this original line
+File 1, Line 1 contains this original line
+File 1, Line 2 contains this original line
+File 1, Line 3 contains this original line
+File 1, Line 4 contains this original line
+File 1, Line 5 contains this original line
+File 1, Line 6 contains this original line
+File 1, Line 7 contains this original line
+File 1, Line 8 contains this original line
+File 1, Line 9 contains this original line
