c54c70e3a06963a3b297f57d3fafd0440816c855
Stephen Jeffrey <stephen.jeffrey@qld.gov.au>
2021-08-12 13:18:43 +1000

    In copy 1: modify file 1

--- file_1.cc
+++ file_1.cc
@@ -1,4 +1,4 @@
-File 1, Line 0 contains this original line
+File 1, Line 0 contains this line (modified in first copy of repo)
 File 1, Line 1 contains this original line
 File 1, Line 2 contains this original line
 File 1, Line 3 contains this original line
